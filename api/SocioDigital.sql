b0a3ff2c815e8d1ebfe44b9bc050eca0b36700cd
André Luís Lopes <161624813+AndreLuisLopes@users.noreply.github.com>
2025-06-18 13:50:23 -0300

    Update SocioDigital.sql

--- api/SocioDigital.sql
+++ api/SocioDigital.sql
@@ -2,23 +2,37 @@ create database sociodigital;
 
 use sociodigital;
 
-create table reservas (
-id int auto_increment not null primary key,
-data date not null
+CREATE TABLE churrasqueira (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50),
+  disponibilidade BOOLEAN DEFAULT TRUE
 );
 
-create table churrasqueira (
-id int auto_increment not null primary key,
-endereco varchar(100) not null
+CREATE TABLE quadra (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50),
+  tipo VARCHAR(30),
+  disponibilidade BOOLEAN DEFAULT TRUE
 );
 
-create table quadra (
-id int auto_increment not null primary key,
-endereco varchar(100) not null,
-tipo varchar(100)
+CREATE TABLE salao (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50),
+  capacidade INT,
+  disponibilidade BOOLEAN DEFAULT TRUE
 );
 
-create table salao (
-id int auto_increment not null primary key,
-endereco varchar(100)
+CREATE TABLE reserva (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usuario VARCHAR(100),
+  tipo_local VARCHAR(20),
+  id_local INT,
+  data_reserva DATE,
+  horario_inicio TIME,
+  horario_fim TIME
 );
+
+select * from salao;
+select * from quadra;
+select * from churrasqueira;
+select * from reserva;
