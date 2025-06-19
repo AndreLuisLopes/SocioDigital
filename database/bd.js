a35fa20d29d9bc5dc25918f0cfd226c0ec163338
André Luís Lopes <161624813+AndreLuisLopes@users.noreply.github.com>
2025-06-19 00:06:35 -0300

    Add files via upload

--- database/bd.js
+++ database/bd.js
@@ -1,7 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const USER_KEY = '@socio_digital_user';
-const RESERVAS_KEY = '@socio_digital_reservas';
 
 export const salvarUsuario = async (usuario) => {
   await AsyncStorage.setItem(USER_KEY, JSON.stringify(usuario));
@@ -10,16 +9,4 @@ export const salvarUsuario = async (usuario) => {
 export const buscarUsuario = async () => {
   const data = await AsyncStorage.getItem(USER_KEY);
   return data ? JSON.parse(data) : null;
-};
-
-export const adicionarReserva = async (reserva) => {
-  const data = await AsyncStorage.getItem(RESERVAS_KEY);
-  const reservas = data ? JSON.parse(data) : [];
-  reservas.push(reserva);
-  await AsyncStorage.setItem(RESERVAS_KEY, JSON.stringify(reservas));
-};
-
-export const listarReservas = async () => {
-  const data = await AsyncStorage.getItem(RESERVAS_KEY);
-  return data ? JSON.parse(data) : [];
 };
