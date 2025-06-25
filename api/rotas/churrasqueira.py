04662abda4c207cfe4fdcad238b4c5f71dacd116
André Luís Lopes <161624813+AndreLuisLopes@users.noreply.github.com>
2025-06-25 16:17:47 -0300

    Add files via upload

--- api/rotas/churrasqueira.py
+++ api/rotas/churrasqueira.py
@@ -5,9 +5,13 @@ churrasqueira_bp = Blueprint('churrasqueira', __name__)
 
 @churrasqueira_bp.route('/churrasqueiras', methods=['GET'])
 def listar_churrasqueiras():
+    clube_id = request.args.get('clube_id')
     conn = conectar()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM churrasqueira")
+    if clube_id:
+        cursor.execute("SELECT * FROM churrasqueira WHERE clube_id = %s", (clube_id,))
+    else:
+        cursor.execute("SELECT * FROM churrasqueira")
     resultado = cursor.fetchall()
     conn.close()
     return jsonify(resultado)
@@ -17,7 +21,10 @@ def criar_churrasqueira():
     dados = request.get_json()
     conn = conectar()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO churrasqueira (nome) VALUES (%s)", (dados['nome'],))
+    cursor.execute(
+        "INSERT INTO churrasqueira (nome, clube_id) VALUES (%s, %s)",
+        (dados['nome'], dados['clube_id'])
+    )
     conn.commit()
     conn.close()
     return jsonify({'mensagem': 'Churrasqueira criada com sucesso!'}), 201
