5b52f578847d92379682139166c113479ecf5298
André Luís Lopes <161624813+AndreLuisLopes@users.noreply.github.com>
2025-06-25 03:33:46 -0300

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -4,6 +4,8 @@ from rotas.reservas import reservas_bp
 from rotas.churrasqueira import churrasqueira_bp
 from rotas.quadra import quadra_bp
 from rotas.salao import salao_bp
+from rotas.usuario import usuario_bp
+from rotas.clube import clube_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +14,8 @@ app.register_blueprint(reservas_bp)
 app.register_blueprint(churrasqueira_bp)
 app.register_blueprint(quadra_bp)
 app.register_blueprint(salao_bp)
+app.register_blueprint(usuario_bp)
+app.register_blueprint(clube_bp)
 
 @app.route('/')
 def home():
@@ -28,6 +32,18 @@ def quadra():
 @app.route('/salao')
 def salao():
     return 'Rota do salão OK!'
+
+@app.route('/usuarios')
+def usuario():
+    return 'Rota do usuário OK!'
+
+@app.route('/clubes')
+def clube():
+    return 'Rota do clube OK!'
+
+@app.route('/clubes/login')
+def login_clube():
+    return 'Rota de login do clube OK!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
