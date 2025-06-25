b7739515ff4cd4bd89d9e02f46faf5ad06d88f80
André Luís Lopes <161624813+AndreLuisLopes@users.noreply.github.com>
2025-06-25 03:34:33 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/login';
 import RegisterScreen from './screens/registro';
 import MainScreen from './screens/main';
+import LoginClube from './screens/loginClube';
+import RegistroClube from './screens/registroClube';
+import Clube from './screens/clube';
+import CadastroEspaco from './screens/cadastroEspaco';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: true }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Registro" component={RegisterScreen} />
-        <Stack.Screen name="Main" component={MainScreen} options={{ title: 'Bem-vindo' }} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: '' }} />
+        <Stack.Screen name="LoginClube" component={LoginClube} options={{ title: '' }} />
+        <Stack.Screen name="Registro" component={RegisterScreen} options={{ title: '' }} />
+        <Stack.Screen name="RegistroClube" component={RegistroClube} options={{ title: '' }} />
+        <Stack.Screen name="Clube" component={Clube} options={{ title: '' }} />
+        <Stack.Screen name="Main" component={MainScreen} options={{ title: '' }} />
+        <Stack.Screen name="CadastroEspaco" component={CadastroEspaco} options={{ title: '' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
